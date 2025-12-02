74b8ca45a327d6fa455cfdd2ef121cfc75b2fd90
Kavya <135150115+CaptainOverride@users.noreply.github.com>
2025-12-02 23:24:14 +0530

    feat(assets): add application icon and configure resources
    - Added [icon.png] and [icon.ico] to a new `resources` directory.
    - Configured Qt Resource System ([resources.qrc]) to bundle the application icon.
    - Updated [main.cpp] to set the window icon at runtime.
    - Added [windows_icon.rc] to [CMakeLists.txt] to embed the icon into the Windows executable (`.exe`).

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,9 +1,12 @@
 #include "mainWindow.h"
 #include <QApplication>
 
+#include <QIcon>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icon.png"));
     MainWindow w;
     w.show();
     return a.exec();
